e8c532825a8e0a292c61e7f18f08b03f3304e3ae
Zang Eric <ezang@lcs-mba3154.lcs.on.ca>
2017-10-27 14:05:15 -0400

    Commit to shut Xcode up

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -4,40 +4,60 @@
 //: Use this page to experiment with a static image.
 //:
 //: When you perfect a technique, you can move it into the animated sketch.
-/*:
- ## Required code
- 
- The following statements are required to make the playground run.
- 
- Please do not remove.
- */
-import Cocoa
-import PlaygroundSupport
+import Foundation
 
-// Create canvas
-let canvas = Canvas(width: 500, height: 500)
+class Sketch : NSObject {
+    
+    // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
+    //       Therefore, the line immediately below must always be present.
+  
+    let canvas : Canvas
+
+    // Position of circle
+    var x : Int
+    
+    // This function runs once
+    override init() {
+        
+        // Create canvas object – specify size
+        canvas = Canvas(width: 500, height: 500)
+        
+        // Set starting position
+        x = 250
+
+        canvas.drawShapesWithBorders = false
+        
+        
+    }
+    
+    // Runs in a loop, forever, to create the animated effect
+    func draw() {
+
+    x += -1
+        
+        // Draw an ellipse bottom of the canvas
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 100, width: 50, height: 50)
+    
+        // Draw an ellipse lower middle of the canvas
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: -x+500, centreY: 200, width: 50, height: 50)
+        
+        // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 300, width: 50, height: 50)
+        
+        // Draw an ellipse top middle of the canvas
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: -x+500, centreY: 400, width: 50, height: 50)
+        
+        // Draw an ellipse at the top of the canvas
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 500, width: 50, height: 50)
+    }
 
 
-var x = 250
-
-
-func draw 
-
-
-
-
-
-
-/*:
- ## Add your code below
- 
- Be sure to write human-readable code.
- 
- Use whitespace and comments as appropriate.
- */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
-
-
+}
 
 /*:
  ## Use source control
@@ -47,17 +67,6 @@ func draw
  
  ![source_control](source_control.png "Source Control")
  */
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right.
- 
- Please do not remove.
- 
- If you don't see output, remember to show the Assistant Editor, and switch to Live View:
- 
- ![timeline](timeline.png "Timeline")
- */
-// Don't remove the code below
-PlaygroundPage.current.liveView = canvas.imageView
+
 
 
