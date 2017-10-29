571da11d2a44a32ddd066959680a7bb1e5355dcd
Zang Eric <ezang@lcs-mba3154.cgocable.net>
2017-10-28 21:07:12 -0400

    Made it more readable

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -33,12 +33,15 @@ class Sketch : NSObject {
         canvas.fillColor = Color.red
         canvas.drawEllipse(centreX: x, centreY: x, width: 50, height: 50)
         
+        //Draw the ellipse going bottom left of the screen
         canvas.fillColor = Color.blue
         canvas.drawEllipse(centreX: x, centreY: 500-x, width: 50, height: 50)
         
+        //Draw the ellipse going to the top left of the screen
         canvas.fillColor = Color.green
         canvas.drawEllipse(centreX: 500-x, centreY: x, width: 50, height: 50)
-        
+     
+        //Draw the ellipse going to the bottom left
         canvas.fillColor = Color.yellow
         canvas.drawEllipse(centreX: 500-x, centreY: 500-x, width: 50, height: 50)
     
