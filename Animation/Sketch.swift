eddcbb61a8392297fee07a2697011a40e94a71da
Zang Eric <ezang@lcs-mba3154.local>
2017-10-31 08:58:33 -0400

    Working on bouncing ball

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -7,27 +7,72 @@ class Sketch : NSObject {
     let canvas : Canvas
     
     // Position of circle
+    
     var x : Int
+    var initialX : Int
+    var initialY : Int
+    var changeInX : Int
+    var changeInY :Int
     
     // This function runs once
     override init() {
         
+        let x = 250
+        
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
         
-        // Set starting position
-        x = 250
-        
+        // Set starting position and starting direction
+
+        var initialX = random(from: 26, toButNotIncluding: 475)
+        var initialY = random(from: 26, toButNotIncluding: 475)
+        var changeInX = random(from: -1, toButNotIncluding: 2)
+        var changeInY = random(from: -1, toButNotIncluding: 2)
+
     }
-    
+    //Initial start and direction
+
+    //
+
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        //Move
+
+        // Change direction
+        if initialX == 500
+        {
+            changeInX = -1
+        }
+        if initialX == 0
+        {
+            changeInX = 1
+        }
+        if initialY == 500
+        {
+            changeInY = -1
+        }
+        if initialY == 0
+        {
+            changeInY = 1
+        }
+        
+        initialX += changeInX
+        initialY += changeInY
+        
+
+        
+        //Wipe the screen
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(centreX: x, centreY: x, width: 500, height: 500)
         
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: initialX, centreY: initialY, width: 50, height: 50)
+        
+
+
         
     }
     
