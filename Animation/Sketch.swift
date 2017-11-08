3eb9bc831a4465c29ac5f3629ad65c979561239d
Zang Eric <ezang@lcs-mba3154.lcs.on.ca>
2017-11-08 09:28:07 -0500

    Finished lines from circles challenge

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,11 +8,19 @@ class Sketch : NSObject {
     
     // Position of circle
     
-
-    var xPosition : Int
-    var yPosition : Int
-    var changeInX : Int
-    var changeInY :Int
+    
+    
+    
+    var x : Double
+    var changeSX : Double
+    var distance : Double
+    var xPosition : Double
+    var yPosition : Double
+    var changeInX : Double
+    var changeInY : Double
+    var h : Int
+    var s : Int
+    var b : Int
     
     // This function runs once
     override init() {
@@ -23,12 +31,16 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position and starting direction
-
-        xPosition = random(from: 26, toButNotIncluding: 475)
-        yPosition = random(from: 26, toButNotIncluding: 475)
-        changeInX = random(from: -5, toButNotIncluding: 6)
-        changeInY = random(from: -5, toButNotIncluding: 6)
-
+        x = 250.0
+        distance = 0.0
+        changeSX = 1.0
+        xPosition = Double(random(from: Int(26.0), toButNotIncluding: Int(475.0)))
+        yPosition = Double(random(from: Int(26.0), toButNotIncluding: Int(475.0)))
+        changeInX = 2.0
+        changeInY = 2.0
+        h = 0
+        b = 0
+        s = 0
     }
     //Initial start and direction
 
@@ -36,44 +48,68 @@ class Sketch : NSObject {
 
     // Runs in a loop, forever, to create the animated effect
     func draw() {
+
         
         //Move
 
+        
         // Change direction
-        if xPosition >= 475
+        if xPosition >= 475.0
         {
-            changeInX = -5
+            changeInX = -2.0
         }
-        if xPosition <= 25
+        if xPosition <= 25.0
         {
-            changeInX = 5
+            changeInX = 2.0
         }
-        if yPosition >= 475
+        if yPosition >= 475.0
         {
-            changeInY = -5
+            changeInY = -2.0
         }
-        if yPosition <= 25
+        if yPosition <= 25.0
         {
-            changeInY = 5
+            changeInY = 2.0
         }
+        //Second circle
+        if x >= 475.0
+        {
+            changeSX = -1.0
+        }
+        if x <= 25.0
+        {
+            changeSX = 1.0
+        }
+
         
         xPosition += changeInX
         yPosition += changeInY
+        x += changeSX
         
-
+//        //Wipe the screen
+//        canvas.drawShapesWithFill = true
+//        canvas.fillColor = Color.white
+//        canvas.drawRectangle(centreX: 250, centreY: 250, width: 500, height: 500)
+//
+//        //Make circles not have fill
+//        canvas.drawShapesWithFill = false
+//        //Wild circle
+//        canvas.fillColor = Color.black
+//        canvas.drawEllipse(centreX: Int(xPosition), centreY: Int(yPosition), width: 100, height: 100)
+//        //Second circle
+//        canvas.drawEllipse(centreX: Int(x), centreY: 250, width: 200, height: 200)
         
-        //Wipe the screen
-        canvas.drawShapesWithBorders = false
-        canvas.fillColor = Color.white
-        canvas.drawRectangle(centreX: 250, centreY: 250, width: 500, height: 500)
+        //Draw the line
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.fillColor = Color.black
-        canvas.drawEllipse(centreX: xPosition, centreY: yPosition, width: 50, height: 50)
+        //Color
+        let h = random(from: 0, toButNotIncluding: 361)
+        let s = random(from: 75, toButNotIncluding: 101)
+        let b = random(from: 50, toButNotIncluding: 101)
+        canvas.lineColor = Color.init(hue: h, saturation: s, brightness: b, alpha: 100)
         
-
-
-        
+        let distance = sqrt(pow(xPosition-x, 2)+pow(yPosition-250, 2))
+        if distance <= 150 {
+            canvas.drawLine(fromX: Int(xPosition), fromY: Int(yPosition), toX: Int(x), toY: 250)
+        }
     }
     
 }
