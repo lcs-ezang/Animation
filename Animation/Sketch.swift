ad7e7df0448e1082f6b6961525dbb66733d8ad90
Zang Eric <ezang@lcs-mba3154.local>
2018-04-12 12:52:07 -0400

    Random commit

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -16,7 +16,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Degrees
-    var y : Double
+    var y : Int
     var Starty : Int
 
     
@@ -29,9 +29,9 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 0.0
-        y = 0.0
-        Starty = 0
+        x = 0
+        y = 0
+        Stary = 0
     }
 
     
@@ -42,20 +42,24 @@ class Sketch : NSObject {
         
         // Change position
         
-        if canvas.frameCount <= 333 {
-        for Starty in stride(from: 0, to: 600, by: 40){
         x += 0.05
-        y = 85*sin(1.5*(Double(x.toRadians())))
+        y = Int(85*sin(1.5*(Double(x.toRadians()))))
+        if canvas.frameCount <= 333
+        {
+        for Starty in stride(from: 0, to: 600, by: 40){
         canvas.drawEllipse(centreX: Int(x), centreY: Starty + Int(y), width: 10, height: 10)
         canvas.drawEllipse(centreX: 500-Int(x), centreY: Starty - Int(y), width: 10, height: 10)
-            }else
-            {
-            
-                x =
-
+        }
+            else {
+        self.x = 50.0
+        self.y = Int(85*sin(1.5*(Double(self.x.toRadians()))))
+        self.canvas.drawEllipse(centreX: Int(self.x), centreY: 50, width: 100, height: 100)
         
-    }
     
+    
+
+
 }
 }
+
 }
